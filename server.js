cf24fc6022b6bc9cef421f66625ad04f96a04203
Jesse Okeya <Jesseokeya@gmail.com>
2017-02-28 07:35:31 -0500

    Third Commit

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const port = process.env.PORT || 5000
+const gzippo = require('gzippo');
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.use('/', express.static('app'));
+app.use(gzippo.staticGzip("" + __dirname + "/app"));
 
 app.listen(port, function(){
   console.log("server listening on port " + port);
