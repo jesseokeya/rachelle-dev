81740517203853b31e5fc98614c649b5597c2a93
Jesse Okeya <Jesseokeya@gmail.com>
2017-03-02 08:19:56 -0500

    Fifth Commit

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,16 @@ var app = angular.module('rachelleApp',
 app.config(function($mdThemingProvider, $mdAriaProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('teal')
-        .accentPalette('red');
+        .accentPalette('cyan');
     $mdAriaProvider.disableWarnings();
+});
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    controller: 'mainController',
+    templateUrl: 'templates/index.tpl.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  })
 });
